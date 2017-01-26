137f0413a74f4415767c249a76c50cc5406a2730
Lukas Backis <2145673B@student.gla.ac.uk>
2017-01-26 16:50:44 +0000

    chapter 6

--- populate_rango.py
+++ populate_rango.py
@@ -1,4 +1,5 @@
 import os
+import random
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       'tango_with_django_project.settings')
 import django
@@ -48,7 +49,7 @@ def populate():
     for cat, cat_data in cats.items():
         c = add_cat(cat)
         for p in cat_data["pages"]:
-            add_page(c, p["title"], p["url"])
+            add_page(c, p["title"], p["url"], random.randrange(0,150))
     
     # Print out the categories we have added.
     for c in Category.objects.all():
@@ -62,7 +63,7 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name, views=128, likes=64):
+def add_cat(name, views=0, likes=0):
     c = Category.objects.get_or_create(name=name)[0]
     c.views=views
     c.likes=likes
